989620bfe2615d9ad86456587903da92612ff3de
liuxl-lin <305211980@qq.com>
2020-01-05 16:46:14 +0800

    提交

---
+++ app/src/main/java/com/liuxl/wifisend/Constant.java
@@ -0,0 +1,9 @@
+package com.liuxl.wifisend;
+
+public interface Constant {
+    String IP="10.10.100.254";
+    int PORT = 8899;
+    String SEND_DATA_1 = "010302000002C5B3";
+
+    String SEND_DATA_2="0110011200020405DC0000BFDC";
+}
